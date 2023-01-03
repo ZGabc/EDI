67b0327e467450ef1d97534e89417e5da97d0cc6
Amanda Miśkiewicz <114912161+amandamiskiewicz@users.noreply.github.com>
2023-01-03 19:14:25 +0100

    Charts

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// uzyte beda promises
+fetch('https://my.api.mockaroo.com/plants.json?key=b98a4910')
+  .then(response => response.json())
+  .then(data => { //data to wszystkie dane z api w formacie json
+    let tableData = '';
 
-fetch('https://my.api.mockaroo.com/plants.json?key=13c557a0').then((data)=>{    //data to wszystkie dane z api
-    return data.json();    //konwertowanie do formatu json, i do obiektu
-}).then((objectData)=>{    //objectData ma dane z api skonwertowane do json
-    let tableData='';
-    objectData.map((values)=>{    //dostajemy sie do kazdego elementu z api
-        tableData+= `<tr>
+    data.map(values => { //dostajemy sie do kazdego elementu z api
+      tableData += `<tr>
         <td>${values.plant_common_name}</td>
         <td>${values.plant_scientific_name}</td>
         <td>${values.plant_family}</td>
@@ -15,8 +14,46 @@ fetch('https://my.api.mockaroo.com/plants.json?key=13c557a0').then((data)=>{    
         <td>${values.lifespan}</td>
         <td>${values.size_in_cm}</td>
         <td>${values.price}</td>
-        </tr>`;
+      </tr>`;
     });
-    document.getElementById("table-body").innerHTML=tableData;
-}) 
+
+    document.getElementById('table-body').innerHTML = tableData;
+
+    //chart1
+    const labels = data.slice(0, 10).map(item => item.plant_common_name); //slice bo pierwsze 10 
+    const values = data.slice(0, 10).map(item => item.size_in_cm); 
+
+    const ctx = document.getElementById('chart1').getContext('2d'); //ctx-context
+    const chart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Size in cm',
+          data: values
+        }]
+      }
+    });
+
+    // chart2
+    const lifespanCounts = data.reduce((counts, plant) => {
+        counts[plant.lifespan] = (counts[plant.lifespan] || 0) + 1;
+        return counts;
+      }, {});
+
+      const lab = Object.keys(lifespanCounts);
+      const val = Object.values(lifespanCounts);
+  
+      const ctx2 = document.getElementById('chart2').getContext('2d');
+      const chart2 = new Chart(ctx2, {
+        type: 'doughnut',
+        data: {
+          labels: lab,
+          datasets: [{
+            label: 'Plant Count',
+            data: val,
+          }]
+        }
+      });
+  });
 
